32c81202262d1729794ad12a6cfd6a970d9f764d
syntextech <ben@syntextech.com>
2015-06-20 02:10:53 -0400

    Header structured

---
+++ header/page_head.php
@@ -0,0 +1,15 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/lib/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/select2/css/select2.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/jquery.bxslider/jquery.bxslider.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/owl.carousel/owl.carousel.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/jquery-ui/jquery-ui.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lib/fancyBox/jquery.fancybox.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css" />
+    
+    <title>Kute shop</title>
---
+++ header/top_banner.php
@@ -0,0 +1,10 @@
+<!-- TOP BANNER -->
+<div id="top-banner" class="top-banner">
+    <div class="bg-overlay"></div>
+    <div class="container">
+        <h1>Special Offer!</h1>
+        <h2>Rewarding all customers with a 15% discount</h2>
+        <span>This offer is available from 9th December to 19th December 2015</span>
+        <span class="btn-close"></span>
+    </div>
+</div>
