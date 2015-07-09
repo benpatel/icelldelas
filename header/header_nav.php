f1c53ceb77dca88b8ddf211335086f43f0ea7dce
Ben Patel <ben@syntextech.com>
2015-07-09 11:34:10 -0400

    Img clss removed

--- header/header_nav.php
+++ header/header_nav.php
@@ -18,7 +18,7 @@ while( $result = $result_set->fetch_object()){
 						<ul class="block">
 							<li class="img_container">
 								<a href="product/'.$result1->id.'/'.str_replace(" ","_",$result1->name).'">
-									<img class="img-responsive img" src="'.SITE_BASE.'scripts/image.php?width=200&amp;height=200&amp;image='.SITE_BASE.'assets/data/01_blue-dress.jpg" alt="sport">
+									<img class="img-responsive" src="'.SITE_BASE.'scripts/image.php?width=200&amp;height=200&amp;image='.SITE_BASE.'assets/data/01_blue-dress.jpg" alt="sport">
 								</a>
 								<a href="product/'.$result1->id.'/'.str_replace(" ","_",$result1->name).'" class="nav_product_image">'.$result1->name.'</a>
 							<p><a href="product/'.$result1->id.'/'.str_replace(" ","_",$result1->name).'" class="btn-add-cart" >Buy Now</a></p>
