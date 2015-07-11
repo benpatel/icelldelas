4ef94b1293e4bd3e49f41fbf190602bf853be0a4
Urvashi Patel <urva@urvaweb.com>
2015-07-11 11:22:46 -0400

    Added a new Class
    do_img class was added to chnage design

--- header/header_nav.php
+++ header/header_nav.php
@@ -16,7 +16,7 @@ while( $result = $result_set->fetch_object()){
 		   while( $result1 = $result_set1->fetch_object()){
 			$list .='<li class="block-container col-sm-3">
 						<ul class="block">
-							<li class="img_container">
+							<li class="img_container do_img">
 								<a href="product/'.$result1->id.'/'.str_replace(" ","_",$result1->name).'">
 									<img class="img-responsive" src="'.SITE_BASE.'scripts/image.php?width=200&amp;height=200&amp;image='.SITE_BASE.'assets/data/01_blue-dress.jpg" alt="sport">
 								</a>
